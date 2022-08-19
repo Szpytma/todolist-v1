0d2cdf4ba97521627db60a522792953583438796
szpytma <szpytmapawel@hotmail.com>
2022-08-19 12:41:43 +0100

    publishing todo list-v1

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  const day = date.getDate();
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.post("/", function (req, res) {
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.listen(port, function () {
+  console.log("App running at port " + port);
+});
