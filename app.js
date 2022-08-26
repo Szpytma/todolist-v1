e91479579127b8688b2e3f63434ab4ae5bb6e26f
szpytma <szpytmapawel@hotmail.com>
2022-08-26 10:32:00 +0100

    adding db

--- app.js
+++ app.js
@@ -2,27 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const port = 3000;
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.static(__dirname + "/public"));
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
 app.get("/", function (req, res) {
   const day = date.getDate();
+
   res.render("list", { listTitle: day, newListItems: items });
-});
-
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
-});
-
-app.get("/about", function (req, res) {
-  res.render("about");
 });
 
 app.post("/", function (req, res) {
@@ -35,6 +30,14 @@ app.post("/", function (req, res) {
     items.push(item);
     res.redirect("/");
   }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(process.env.PORT || port);
